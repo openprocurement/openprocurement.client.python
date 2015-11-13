e2f425236a079bd03065cc42459d58b57ab4caa0
selurvedu <selurvedu@users.noreply.github.com>
2015-11-13 21:03:57 +0000

    Re-indent setup.py

--- setup.py
+++ setup.py
@@ -3,37 +3,38 @@ import os
 
 version = '1.0a2'
 
-setup(name='openprocurement_client',
-      version=version,
-      description="",
-      long_description="{0}\n{1}".format(
-          open("README.md").read(),
-          open(os.path.join("docs", "HISTORY.txt")).read()
-      ),
-      # Get more strings from
-      # http://pypi.python.org/pypi?:action=list_classifiers
-      classifiers=[
-          "Programming Language :: Python",
-      ],
-      keywords='',
-      author='',
-      author_email='',
-      url='http://svn.plone.org/svn/collective/',
-      license='GPL',
-      packages=find_packages(exclude=['ez_setup']),
-      include_package_data=True,
-      zip_safe=False,
-      install_requires=[
-          'setuptools',
-          'gevent',
-          'restkit',
-          'munch',
-          'simplejson',
-          'retrying'
-          # -*- Extra requirements: -*-
-      ],
-      entry_points="""
-      # -*- Entry points: -*-
-      """,
-      test_suite="openprocurement_client.tests"
-      )
+setup(
+    name='openprocurement_client',
+    version=version,
+    description="",
+    long_description="{0}\n{1}".format(
+        open("README.md").read(),
+        open(os.path.join("docs", "HISTORY.txt")).read()
+    ),
+    # Get more strings from
+    # http://pypi.python.org/pypi?:action=list_classifiers
+    classifiers=[
+        "Programming Language :: Python",
+    ],
+    keywords='',
+    author='',
+    author_email='',
+    url='http://svn.plone.org/svn/collective/',
+    license='GPL',
+    packages=find_packages(exclude=['ez_setup']),
+    include_package_data=True,
+    zip_safe=False,
+    install_requires=[
+        'setuptools',
+        'gevent',
+        'restkit',
+        'munch',
+        'simplejson',
+        'retrying'
+        # -*- Extra requirements: -*-
+    ],
+    entry_points="""
+    # -*- Entry points: -*-
+    """,
+    test_suite="openprocurement_client.tests"
+)
