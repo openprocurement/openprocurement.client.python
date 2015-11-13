8fdc20e72f2dc4f2aa81724d4b999ea0d2e849a8
selurvedu <selurvedu@users.noreply.github.com>
2015-11-13 21:21:38 +0000

    Split setup.py into blocks; sort install_requires

--- setup.py
+++ setup.py
@@ -11,26 +11,29 @@ setup(
         open("README.md").read(),
         open(os.path.join("docs", "HISTORY.txt")).read()
     ),
+
     # Get more strings from
     # http://pypi.python.org/pypi?:action=list_classifiers
     classifiers=[
         "Programming Language :: Python",
     ],
     keywords='',
+
     author='',
     author_email='',
     url='http://svn.plone.org/svn/collective/',
     license='GPL',
+
     packages=find_packages(exclude=['ez_setup']),
     include_package_data=True,
     zip_safe=False,
+
     install_requires=[
-        'setuptools',
         'gevent',
-        'restkit',
         'munch',
-        'simplejson',
-        'retrying'
+        'restkit',
+        'retrying',
+        'simplejson'
         # -*- Extra requirements: -*-
     ],
     entry_points="""
