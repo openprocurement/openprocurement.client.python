be94d4c6c5cd0c0999abca8b084cc87a64e9a8d0
Volodymyr Maksymiv <vmaksymiv@quintagroup.com>
2016-04-25 19:05:15 +0300

    add contracting client

---
+++ openprocurement_client/contract.py
@@ -0,0 +1,30 @@
+from client import APIBaseClient, verify_file
+
+
+class ContractingClient(APIBaseClient):
+    """ contracting client """
+
+    def __init__(self, key,
+                 host_url="https://api-sandbox.openprocurement.org",
+                 api_version='2.0',
+                 params=None):
+        super(ContractingClient, self).__init__(key, host_url, api_version,
+                                                "contracts", params)
+
+    @verify_file
+    def upload_document(self, file_, contract):
+        return self._upload_resource_file(
+            '{}/{}/documents'.format(
+                self.prefix_path,
+                contract.data.id
+            ),
+            data={"file": file_},
+            headers={'X-Access-Token':
+                     getattr(getattr(contract, 'access', ''), 'token', '')}
+        )
+
+    def create_contract(self, contract):
+        return self._create_resource_item(self.prefix_path, contract)
+
+    def get_contract(self, id):
+        return self._get_resource_item('{}/{}'.format(self.prefix_path, id))
