89fc2f17c3fa2f448398f8af2d14f76d91bd604b
VTinkovan <tinkovan.v@gmail.com>
2023-08-21 14:08:06 +0200

    added upload_framework_document

--- openprocurement_client/resources/frameworks.py
+++ openprocurement_client/resources/frameworks.py
@@ -3,7 +3,7 @@ import logging
 from retrying import retry
 
 from openprocurement_client.clients import APIResourceClient
-from openprocurement_client.constants import FRAMEWORKS
+from openprocurement_client.constants import (FRAMEWORKS, DOCUMENTS)
 
 LOGGER = logging.getLogger(__name__)
 
@@ -25,17 +25,29 @@ class FrameworksClient(APIResourceClient):
     #             CREATE ITEM API METHODS
     ###########################################################################
 
-    def create_qualification(self, qualification):
-        return self.create_resource_item(qualification)
+    def create_qualification(self, framework):
+        return self.create_resource_item(framework)
 
     ###########################################################################
     #             GET ITEM API METHODS
     ###########################################################################
 
     @retry(stop_max_attempt_number=5)
-    def get_qualification(self, qualification_id):
-        return self.get_resource_item(qualification_id)
+    def get_qualification(self, framework_id):
+        return self.get_resource_item(framework_id)
 
     ###########################################################################
     #             PATCH ITEM API METHODS
     ###########################################################################
+
+
+    ###########################################################################
+    #             UPLOAD FILE API METHODS
+    ###########################################################################
+    def upload_framework_document(self, file, framework_id, use_ds_client=True,
+                              doc_registration=True, access_token=None):
+        depth_path = '{}'.format(DOCUMENTS)
+        return self.upload_document(file, framework_id,
+                                    use_ds_client=use_ds_client,
+                                    doc_registration=doc_registration,
+                                    depth_path=depth_path, access_token=access_token)
