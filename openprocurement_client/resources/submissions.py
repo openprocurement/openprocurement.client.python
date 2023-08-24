c5fd4f9bcf27756d3d23ca0375ab495971bbb857
VTinkovan <tinkovan.v@gmail.com>
2023-08-24 09:13:02 +0200

    added patch_submission

--- openprocurement_client/resources/submissions.py
+++ openprocurement_client/resources/submissions.py
@@ -14,6 +14,9 @@ class SubmissionsClient(APIResourceClient):
     def create_submission(self, submission):
         return self.create_resource_item(submission)
 
+    def patch_submission(self, submission_id, patch_data={}, access_token=None):
+        return self.patch_resource_item(submission_id, patch_data, access_token=access_token)
+
     def upload_submission_document(self, file, submission_id, use_ds_client=True,
                               doc_registration=True, access_token=None):
         return self.upload_document(file, submission_id,
